887abd168af39ad620e3004dcedcaa0824e4f155
GARDENJK1233 <roasamuel1233@gmail.com>
2025-06-02 23:14:42 -0500

    Parte 5
    "Solucion" error cambio de escena

--- Alpina/Assets/Scripts/Objects/LevelFinish.cs
+++ Alpina/Assets/Scripts/Objects/LevelFinish.cs
@@ -2,29 +2,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelFinish : MonoBehaviour
 {
-    // Lista de nombres de las escenas en el orden en que deben ser cargadas
-    public string[] sceneNames;
-    private void OnTriggerEnter(Collider other)
+    
+    // Nombre de la escena a la que deseas cambiar
+    public string sceneToLoad;
+    void Update()
     {
-        if (other.CompareTag("Player"))
+        // Cambiar de escena al presionar la tecla "C"
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            Debug.Log("Jugador llegó a la meta. Avanzando al siguiente nivel...");
-            LoadNextLevel();
+            ChangeScene();
         }
     }
-    void LoadNextLevel()
+    void ChangeScene()
     {
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        int currentSceneIndex = System.Array.IndexOf(sceneNames, currentSceneName);
-        if (currentSceneIndex >= 0 && currentSceneIndex < sceneNames.Length - 1)
-        {
-            string nextSceneName = sceneNames[currentSceneIndex + 1];
-            SceneManager.LoadScene(nextSceneName);
-        }
-        else
-        {
-            Debug.Log("Último nivel alcanzado. No hay más niveles.");
-            // Aquí puedes agregar lógica para mostrar un menú o reiniciar el juego
-        }
+        // Cargar la escena especificada
+        SceneManager.LoadScene(sceneToLoad);
     }
+
 }
